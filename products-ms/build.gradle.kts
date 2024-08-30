2331342b3ffb0944c5b7916b514d8453d4bdee94
luans <luanssantos12@gmail.com>
2024-08-30 09:27:09 -0300

    feat: Exposição e exibição das métricas da aplicação

--- products-ms/build.gradle.kts
+++ products-ms/build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("io.micrometer:micrometer-registry-prometheus:1.13.3")
+    implementation("io.micrometer:micrometer-registry-prometheus-simpleclient:1.13.3")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.0")
