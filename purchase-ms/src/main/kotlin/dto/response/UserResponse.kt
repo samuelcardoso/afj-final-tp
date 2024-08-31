565075d88e221590a3ae080a4532f1a88676c957
José Carlos Pereira Silva Filho <jcps.filho@gmail.com>
2024-08-31 09:56:07 -0300

    Adding id to user response

--- purchase-ms/src/main/kotlin/dto/response/UserResponse.kt
+++ purchase-ms/src/main/kotlin/dto/response/UserResponse.kt
@@ -1,8 +1,9 @@
 package puc.dto.response
 
 data class UserResponse(
+    val id: Long?,
     val username: String,
     val roles: Set<String>
 ){
-    constructor() : this("", emptySet())
+    constructor() : this(0L, "", emptySet())
 }
