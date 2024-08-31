218c36167b0efab705f21346a83cd88f0ab34436
José Carlos Pereira Silva Filho <jcps.filho@gmail.com>
2024-08-31 11:01:32 -0300

    Moving bussines rules to a service class

--- purchase-ms/src/main/kotlin/puc/purchase/PurchaseController.kt
+++ purchase-ms/src/main/kotlin/puc/purchase/PurchaseController.kt
@@ -1,38 +1,33 @@
 package puc.purchase
 
+import com.fasterxml.jackson.core.JsonProcessingException
 import org.springframework.web.bind.annotation.*
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
 import puc.util.JwtUtil
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.amqp.AmqpException
+import puc.model.PurchaseRequest
+import puc.service.PurchaseService
 
 @RestController
 @RequestMapping("/purchase")
-class PurchaseController(val rabbitTemplate: RabbitTemplate, val jwtUtil: JwtUtil) {
-
-    @Value("\${rabbitmq.exchange}")
-    lateinit var exchange: String
-
-    @Value("\${rabbitmq.routingkey}")
-    lateinit var routingKey: String
+class PurchaseController(val purchaseService: PurchaseService, val jwtUtil: JwtUtil) {
 
     @PostMapping("/buy")
     fun buy(@RequestHeader("Authorization") token: String, @RequestBody purchaseRequest: PurchaseRequest): ResponseEntity<String> {
-        val userId: Long
-
         try {
-            userId = jwtUtil.getUserId(token)
-        } catch (e: Exception) {
+            val userId = jwtUtil.getUserId(token)
+            purchaseService.sendMessage(purchaseRequest, userId)
+        }catch (e: AmqpException) {
+            return ResponseEntity.internalServerError().body("Failed to send message to MQ");
+        } catch (e: JsonProcessingException) {
+            return ResponseEntity.internalServerError().body("Failed to process json");
+        } catch (e: RuntimeException) {
             return ResponseEntity.status(401).body(e.message);
         }
 
-        val purchaseMessage = PurchaseMessage(userId, purchaseRequest.productId, purchaseRequest.quantity)
-        val objectMapper = ObjectMapper()
-        val messageAsString = objectMapper.writeValueAsString(purchaseMessage)
-        rabbitTemplate.convertAndSend(exchange, routingKey, messageAsString)
         return ResponseEntity.ok("Purchase request sent.")
     }
 }
-
-data class PurchaseRequest(val productId: String, val quantity: Int)
