092aa3830afa50da8836dadb181b5f7cd7ba7608
irlaandrade <paula.irla@gmail.com>
2024-09-01 18:35:24 -0300

    feat: fix retry config

--- purchase-ms/src/main/kotlin/puc/config/RabbitMQConfig.kt
+++ purchase-ms/src/main/kotlin/puc/config/RabbitMQConfig.kt
@@ -2,12 +2,14 @@ package puc.config
 
 import org.springframework.amqp.core.ExchangeBuilder
 import org.springframework.amqp.core.Queue
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory
 import org.springframework.amqp.rabbit.connection.ConnectionFactory
 import org.springframework.amqp.rabbit.core.RabbitAdmin
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
-//import org.springframework.amqp.rabbit.listener.SimpleRabbitListenerContainerFactory
 import org.springframework.context.annotation.Configuration
+import org.springframework.retry.policy.SimpleRetryPolicy
+import org.springframework.retry.support.RetryTemplate
 
 @Configuration
 class RabbitMQConfig {
@@ -36,20 +38,20 @@ class RabbitMQConfig {
         return RabbitAdmin(connectionFactory)
     }
 
-//    @Bean
-//    fun simpleRabbitListenerContainerFactory(connectionFactory: ConnectionFactory): SimpleRabbitListenerContainerFactory {
-//        val retryTemplate = RetryTemplate()
-//        val retryPolicy = SimpleRetryPolicy(3)
-//        retryTemplate.retryPolicy = retryPolicy
-//
-//        val factory = SimpleRabbitListenerContainerFactory()
-//        factory.connectionFactory = connectionFactory
-//        factory.setConcurrentConsumers(3)
-//        factory.setMaxConcurrentConsumers(10)
-//        factory.setPrefetchCount(1)
-//        factory.setRetryTemplate(retryTemplate)
-//        return factory
-//    }
+    @Bean
+    fun simpleRabbitListenerContainerFactory(connectionFactory: ConnectionFactory): SimpleRabbitListenerContainerFactory {
+        val retryTemplate = RetryTemplate()
+        val retryPolicy = SimpleRetryPolicy(3)
+        retryTemplate.setRetryPolicy(retryPolicy)
+
+        val factory = SimpleRabbitListenerContainerFactory()
+        factory.setConnectionFactory(connectionFactory)
+        factory.setConcurrentConsumers(3)
+        factory.setMaxConcurrentConsumers(10)
+        factory.setPrefetchCount(1)
+        factory.setRetryTemplate(retryTemplate)
+        return factory
+    }
 
     @Bean
     fun binding(): org.springframework.amqp.core.Binding {
