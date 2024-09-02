caee0ee2040dd24b44671bcfaf344f63dac87e25
Atílio Ferreira <atilio.silvaferreira@gmail.com>
2024-09-02 18:57:03 -0300

    test:add unit test for getMe, updateUser and changePassword controllers

--- user-ms/src/test/kotlin/puc/controller/UserAppControllerTest.kt
+++ user-ms/src/test/kotlin/puc/controller/UserAppControllerTest.kt
@@ -1,5 +1,5 @@
 package puc.controller
-import java.net.URI
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -10,16 +10,16 @@ import org.mockito.Mockito
 import org.mockito.junit.jupiter.MockitoExtension
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import puc.exception.custom.InvalidCredentialsException
-import puc.exception.custom.UserNotFoundException
-import puc.exception.custom.UsernameAlreadyExistsException
-import puc.model.dto.request.LoginRequest
-import puc.model.dto.request.RefreshTokenRequest
-import puc.model.dto.request.RegisterRequest
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.context.SecurityContextHolder
+import puc.exception.custom.*
+import puc.model.dto.request.*
 import puc.model.dto.response.LoginResponse
 import puc.model.dto.response.RefreshTokenResponse
 import puc.model.dto.response.UserResponse
 import puc.service.UserAppService
+import java.net.URI
 
 @ExtendWith(MockitoExtension::class)
 class UserAppControllerTest {
@@ -36,6 +36,8 @@ class UserAppControllerTest {
     private lateinit var loginResponse: LoginResponse
     private lateinit var refreshTokenRequest: RefreshTokenRequest
     private lateinit var refreshTokenResponse: RefreshTokenResponse
+    private lateinit var updateUserRequest: UpdateUserRequest
+    private lateinit var changePasswordRequest: ChangePasswordRequest
 
     @BeforeEach
     fun setUp() {
@@ -83,6 +85,34 @@ class UserAppControllerTest {
             tokenExpiresIn = 3600L,
             refreshToken = "exampleRefreshToken"
         )
+
+        updateUserRequest = UpdateUserRequest(
+            roles = setOf("ROLE_USER"),
+            document = "1234567890",
+            firstName = "New",
+            lastName = "User",
+            email = "newuser@example.com",
+            phone = "1234567890"
+        )
+
+        changePasswordRequest = ChangePasswordRequest(
+            oldPassword = "StrongPassword123!",
+            newPassword = "NewStrongPassword!456"
+        )
+
+        val principal = org.springframework.security.core.userdetails.User(
+            "user",
+            "pass",
+            listOf(SimpleGrantedAuthority("ROLE_USER"))
+        )
+        val auth = UsernamePasswordAuthenticationToken(principal, null, listOf(SimpleGrantedAuthority("ROLE_USER")))
+
+        SecurityContextHolder.getContext().authentication = auth
+    }
+
+    @AfterEach
+    fun tearDown() {
+        SecurityContextHolder.clearContext()
     }
 
     @Test
@@ -139,6 +169,28 @@ class UserAppControllerTest {
         assertEquals("Invalid credentials", exception.message)
     }
 
+
+    @Test
+    fun `getMe should return user infos`() {
+        Mockito.`when`(userService.getUserInfo("user")).thenReturn(userResponse)
+
+        val response: ResponseEntity<UserResponse> = userAppController.getMe()
+
+        assertEquals(HttpStatus.OK, response.statusCode)
+        assertEquals(userResponse, response.body)
+    }
+
+    @Test
+    fun `getMe should throw UserNotFoundException when username no exists`() {
+        Mockito.`when`(userService.getUserInfo("user")).thenThrow(UserNotFoundException("User not found"))
+
+        val exception = org.junit.jupiter.api.assertThrows<UserNotFoundException> {
+            userAppController.getMe()
+        }
+
+        assertEquals("User not found", exception.message)
+    }
+
     @Test
     fun `refreshToken should return a new token`() {
         Mockito.`when`(userService.refreshToken(refreshTokenRequest.refreshToken)).thenReturn(refreshTokenResponse)
@@ -169,5 +221,99 @@ class UserAppControllerTest {
         }
 
         assertEquals("User not found", exception.message)
+    }
+
+    @Test
+    fun `updateUser should update the user infos`() {
+        Mockito.`when`(userService.updateUser(1L, updateUserRequest, "user", updateUserRequest.roles)).thenReturn(userResponse)
+
+        val response: ResponseEntity<UserResponse> = userAppController.updateUser(1L, updateUserRequest)
+
+        assertEquals(HttpStatus.OK, response.statusCode)
+        assertEquals(userResponse, response.body)
+    }
+
+    @Test
+    fun `updateUser should throw UserNotFoundException when username no exists`() {
+        Mockito.`when`(userService.updateUser(1L, updateUserRequest, "user", updateUserRequest.roles)).thenThrow(UserNotFoundException("User not found"))
+
+        val exception = org.junit.jupiter.api.assertThrows<UserNotFoundException> {
+            userAppController.updateUser(1L, updateUserRequest)
+        }
+
+        assertEquals("User not found", exception.message)
+    }
+
+    @Test
+    fun `updateUser should throw InvalidRoleException when role is invalid`() {
+        Mockito.`when`(userService.updateUser(1L, updateUserRequest, "user", updateUserRequest.roles)).thenThrow(
+            InvalidRoleException("User with ROLE_USER can only update themselves")
+        )
+
+        val exception = org.junit.jupiter.api.assertThrows<InvalidRoleException> {
+            userAppController.updateUser(1L, updateUserRequest)
+        }
+
+        assertEquals("User with ROLE_USER can only update themselves", exception.message)
+    }
+
+    @Test
+    fun `updateUser should throw InvalidRoleException when new role is invalid`() {
+        Mockito.`when`(userService.updateUser(1L, updateUserRequest, "user", updateUserRequest.roles)).thenThrow(
+            InvalidRoleException("User with ROLE_USER can only have ROLE_USER")
+        )
+
+        val exception = org.junit.jupiter.api.assertThrows<InvalidRoleException> {
+            userAppController.updateUser(1L, updateUserRequest)
+        }
+
+        assertEquals("User with ROLE_USER can only have ROLE_USER", exception.message)
+    }
+
+    @Test
+    fun `updateUser should throw UserEmailAlreadyExistsException when new email already exist`() {
+        Mockito.`when`(userService.updateUser(1L, updateUserRequest, "user", updateUserRequest.roles)).thenThrow(
+            UserEmailAlreadyExistsException("User already exists with the email %s".format(updateUserRequest.email))
+        )
+
+        val exception = org.junit.jupiter.api.assertThrows<UserEmailAlreadyExistsException> {
+            userAppController.updateUser(1L, updateUserRequest)
+        }
+
+        assertEquals("User already exists with the email %s".format(updateUserRequest.email), exception.message)
+    }
+
+    @Test
+    fun `changePassword should update the user password`() {
+        Mockito.`when`(userService.changePassword("user", changePasswordRequest)).thenReturn(userResponse)
+
+        val response: ResponseEntity<UserResponse> = userAppController.changePassword(changePasswordRequest)
+
+        assertEquals(HttpStatus.OK, response.statusCode)
+        assertEquals(userResponse, response.body)
+    }
+
+    @Test
+    fun `changePassword should throw UserNotFoundException when username no exists`() {
+        Mockito.`when`(userService.changePassword("user", changePasswordRequest)).thenThrow(UserNotFoundException("User not found"))
+
+        val exception = org.junit.jupiter.api.assertThrows<UserNotFoundException> {
+            userAppController.changePassword(changePasswordRequest)
+        }
+
+        assertEquals("User not found", exception.message)
+    }
+
+    @Test
+    fun `changePassword should throw InvalidCredentialsException when old password do not match`() {
+        Mockito.`when`(userService.changePassword("user", changePasswordRequest)).thenThrow(
+            InvalidCredentialsException("Invalid old password")
+        )
+
+        val exception = org.junit.jupiter.api.assertThrows<InvalidCredentialsException> {
+            userAppController.changePassword(changePasswordRequest)
+        }
+
+        assertEquals("Invalid old password", exception.message)
     }
 }
