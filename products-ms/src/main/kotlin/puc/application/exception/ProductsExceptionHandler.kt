6f66990c7bcb243fce32cc0a5ea6ce48c2d90683
luans <luanssantos12@gmail.com>
2024-09-03 22:05:27 -0300

    feat: Merging main

--- products-ms/src/main/kotlin/puc/application/exception/ProductsExceptionHandler.kt
+++ products-ms/src/main/kotlin/puc/application/exception/ProductsExceptionHandler.kt
@@ -6,6 +6,7 @@ import org.springframework.http.ProblemDetail
 import org.springframework.security.access.AccessDeniedException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.server.ResponseStatusException
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 import puc.domain.products.services.exceptions.ProductNotFoundException
 import software.amazon.awssdk.services.acm.model.ResourceNotFoundException
