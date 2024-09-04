70c1950c016d610e76e3425dc9d6c2e9048d4dc9
José Carlos Pereira Silva Filho <jcps.filho@gmail.com>
2024-09-03 21:18:36 -0300

    Removing unecessary code

--- purchase-ms/src/main/kotlin/puc/controller/PurchaseController.kt
+++ purchase-ms/src/main/kotlin/puc/controller/PurchaseController.kt
@@ -1,26 +1,16 @@
 package puc.controller
 
+import com.fasterxml.jackson.core.JsonProcessingException
+import jakarta.validation.Valid
 import org.springframework.amqp.AmqpException
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestHeader
-import org.springframework.web.bind.annotation.RequestBody
-import com.fasterxml.jackson.core.JsonProcessingException
-import puc.gateway.UserMsRestTemplate
-import puc.model.PurchaseRequest
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
-import puc.service.PurchaseService
-import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.*
 import puc.exceptions.ErrorTryingSoSendMessageMQException
 import puc.exceptions.ErrorTryingToProcessJSONException
-import kotlinx.serialization.encodeToString
+import puc.gateway.UserMsRestTemplate
+import puc.model.PurchaseRequest
+import puc.service.PurchaseService
 import puc.vo.JWT
-
-@Serializable
-data class ErrorGateway(val timestamp: String, val status: Int, val error: String, val path: String)
 
 @RestController
 @RequestMapping("/purchase")
